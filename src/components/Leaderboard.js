d6f03034bf4e2314dd9c5851856f5acd4543a18a
Tim Dill <brodt7258@gmail.com>
2018-05-12 04:32:08 -0400

    basic working leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Leaderboard extends Component {
   render() {
+    const { users } = this.props;
+
     return (
-      <div>Leaderboard</div>
+      <div>
+        <div>Leaderboard</div>
+        <br/>
+        {
+          users.map(u => (
+            <div key={u.id}>
+              {`${u.name}  |  ${u.score}`}
+            </div>
+          ))
+        }
+      </div>
     );
   }
 }
 
-export default Leaderboard;
+const mapStateToProps = ({ users }) => {
+  return {
+    users: users 
+    ? Object.values(users)
+      .map(({ id, name, answers, questions }) => ({ id, name, score: questions.length + Object.keys(answers).length  }))
+      .sort((a, b) => b.score - a.score)
+    : []
+  }
+};
+
+export default connect(mapStateToProps)(Leaderboard);
